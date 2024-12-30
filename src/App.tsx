18e5f6d129c43c311e5641c18778086055b6b85c
pogorielova <nst.pogorielova@gmail.com>
2024-12-30 14:19:06 +0200

    create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
-import classNames from 'classnames';
-
-const getLinkClass = ({ isActive }: { isActive: boolean }) =>
-  classNames('navbar-item', { 'has-background-grey-lighter': isActive });
+import { Navbar } from './components/Navbar/Navbar';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink className={getLinkClass} to="/">
-            Home
-          </NavLink>
-
-          <NavLink className={getLinkClass} to="/people">
-            People
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     <main className="section">
       <div className="container">
